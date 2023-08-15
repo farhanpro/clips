01174e6b4984562306ce2d2553b43b1288a90ccc
farhanpro <shaikhfarhan549@gmail.com>
2023-08-16 03:08:02 +0530

    Form's Binded

--- src/app/shared/modal/modal.component.ts
+++ src/app/shared/modal/modal.component.ts
@@ -1,4 +1,4 @@
-import { Component,Input } from '@angular/core';
+import { Component,Input,ElementRef ,OnInit} from '@angular/core';
 import { ModalService } from 'src/app/services/modal.service';
 @Component({
   selector: 'app-modal',
@@ -8,8 +8,14 @@ import { ModalService } from 'src/app/services/modal.service';
 })
 export class ModalComponent {
   @Input() modalID =''
-  constructor(public modal: ModalService) {}
+  constructor(public modal: ModalService,public el : ElementRef) {
+    console.log(el);
+  }
+  ngOnInit():void{
+    document.body.appendChild(this.el.nativeElement);
+  }
   closeModal() {
     this.modal.toggalModal(this.modalID);
   }
+
 }
